4ae240c8a0284963534d032ed699d9a70c743113
Phillip <phillip_fry@umail.ucsb.edu>
2016-01-29 10:07:05 -0800

    update

--- src/edu/ucsb/cs56/w16/drawings/phillip_fry/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w16/drawings/phillip_fry/advanced/AllMyDrawings.java
@@ -23,20 +23,20 @@ public class AllMyDrawings
     
     public static void drawPicture1(Graphics2D g2) {
 	
-	Tank h1 = new Tank(100,250,100,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Tank t1 = new Tank(100,250,100,75);
+	g2.setColor(Color.CYAN); g2.draw(t1);
 	
 	// Make a black house that's half the size, 
 	// and moved over 150 pixels in x direction
 	
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape t2 = ShapeTransforms.scaledCopyOfLL(t1,0.5,0.5);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(t2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	t2 = ShapeTransforms.scaledCopyOfLL(t2,4,4);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -48,15 +48,15 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(t2); 
 	
 	// Draw two houses with Windows
 	
-	flagTank hw1 = new flagTank(50,100,50,60);
-	flagTank hw2 = new flagTank(200,500,200,100);
+	flagTank fT1 = new flagTank(50,100,50,60);
+	flagTank fT2 = new flagTank(200,500,200,100);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(fT1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(fT2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
@@ -82,19 +82,19 @@ public class AllMyDrawings
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Tank t1 = new Tank(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(t1);
 	
 	// Make a black house that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape t2 = ShapeTransforms.scaledCopyOfLL(t1,0.5,0.5);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(t2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	t2 = ShapeTransforms.scaledCopyOfLL(t2,4,4);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -106,20 +106,20 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(t2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	flagTank fT1 = new flagTank(50,350,40,75);
+	flagTank fT2 = new flagTank(200,350,200,100);
 	
-	g2.draw(hw1);
+	g2.draw(fT1);
 	g2.setColor(new Color(0x8F00FF)); 
 	
 	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	Shape fT3 = ShapeTransforms.rotatedCopyOf(fT2, Math.PI/4.0);
 	
-	g2.draw(hw3);
+	g2.draw(fT3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
@@ -140,8 +140,8 @@ public class AllMyDrawings
 	
 	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+	Tank large = new Tank(100,50,225,150);
+	flagTank smallCC = new flagTank(20,50,40,30);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
 	g2.setColor(Color.GREEN);   g2.draw(smallCC);
